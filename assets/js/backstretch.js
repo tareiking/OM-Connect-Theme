724a3839c3983b7849cc1daef22b60413abd5e61
Tarei King <tareiking@gmail.com>
2014-08-25 23:39:21 +1000

    Remove console log from backstretch

--- assets/js/backstretch.js
+++ assets/js/backstretch.js
@@ -1,7 +1,4 @@
 
 (function($) {
-	$( document ).ready(function() {
-		console.log( om_connect_custom_vars.background );
-		$('.custom-background').backstretch( om_connect_custom_vars.background );
-	});
+	$('.custom-background').backstretch( om_connect_custom_vars.background );
 })(jQuery);
